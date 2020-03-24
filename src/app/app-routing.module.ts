d5e671ecf4df23977dfe88dd745ca99d2c852df9
viniciusjps <vinicius.jorge.silva@ccc.ufcg.edu.br>
2020-03-24 16:30:31 -0300

    add nova rota

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,21 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
+import { RegisterComponent } from './pages/auth/register/register.component';
 
 const routes: Routes = [
   {
     path: '',
     component: HomeComponent,
+  },
+  {
+    path: 'user',
+    children: [
+      {
+        path: 'new',
+        component: RegisterComponent
+      }
+    ]
   },
   {
     path: '**',
