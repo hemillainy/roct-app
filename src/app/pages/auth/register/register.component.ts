a5aeaa07b9f0591bcaed2f15fe9735cad8830057
Vinicius Jorge <vinicius.jorge.silva@ccc.ufcg.edu.br>
2020-03-28 19:52:57 +0000

    adicionar selação de avatar

--- src/app/pages/auth/register/register.component.ts
+++ src/app/pages/auth/register/register.component.ts
@@ -10,20 +10,44 @@ export class RegisterComponent implements OnInit {
   // Data
   public data: any;
 
+  // Scenes
+  public changeAvatar: boolean;
+  public boxAvatar: boolean;
+
+
   constructor() {
     this.data = {
       name: undefined,
       username: undefined,
       cpf: undefined,
       email: undefined,
+      avatar: undefined,
       pwd: {
         password: undefined,
         confirm_password: undefined
       }
     };
+    this.changeAvatar = false;
+    this.boxAvatar = false;
   }
 
   ngOnInit() {
+  }
+
+  public openAvatarBox(): void {
+    this.boxAvatar = true;
+  }
+
+  public closeAvatarBox(): void {
+    this.boxAvatar = false;
+  }
+
+  public setAvatar(event): void {
+    this.data.avatar = event.target.src;
+    this.closeAvatarBox();
+  }
+
+  public submit(): void {
   }
 
 }
