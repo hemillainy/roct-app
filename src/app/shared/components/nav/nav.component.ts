1165eb3332e2fa6ae1affdbc6d6769beae9b779e
Vinicius Jorge <vinicius.jorge.silva@ccc.ufcg.edu.br>
2020-09-26 21:13:01 +0000

    add navbar

--- src/app/shared/components/nav/nav.component.ts
+++ src/app/shared/components/nav/nav.component.ts
@@ -1,15 +1,31 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-nav',
   templateUrl: './nav.component.html',
-  styleUrls: ['./nav.component.sass']
+  styleUrls: ['./nav.component.scss']
 })
 export class NavComponent implements OnInit {
 
-  constructor() { }
+  private scroll = 0;
+
+  constructor(
+    private router: Router
+  ) { }
 
   ngOnInit() {
+  }
+
+  @HostListener('window:scroll', ['$event'])
+  public fixed(event: any): boolean {
+    const st = window.pageYOffset || document.documentElement.scrollTop;
+    this.scroll = st <= 0 ? 0 : st;
+    return st > 200;
+  }
+
+  public show(): boolean {
+    return !this.router.url.includes('user/login') && !this.router.url.includes('user/new');
   }
 
 }
