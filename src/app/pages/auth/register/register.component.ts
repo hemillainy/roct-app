397dd747063e9eb8da629197d87fb534f6e0d072
Gabriel <gabriel.almeida.azevedo@ccc.ufcg.edu.br>
2020-10-16 15:27:07 -0300

    Nova tela de cadastro

--- src/app/pages/auth/register/register.component.ts
+++ src/app/pages/auth/register/register.component.ts
@@ -29,6 +29,7 @@ export class RegisterComponent implements OnInit {
   // Scenes
   public changeAvatar: boolean;
   public boxAvatar: boolean;
+  public vendedor: boolean;
 
   constructor(
     private session: SessionService,
@@ -37,9 +38,9 @@ export class RegisterComponent implements OnInit {
   ) {
     this.data = {
       name: undefined,
-      nickname: undefined,
-      cpf: undefined,
       email: undefined,
+      cpf: undefined,
+      telefone: undefined,
       avatar: undefined,
       pwd: {
         password: undefined,
@@ -48,9 +49,11 @@ export class RegisterComponent implements OnInit {
     };
     this.changeAvatar = false;
     this.boxAvatar = false;
+    this.vendedor = false;
     this.status = {
       loading: false,
-      error: false
+      error: false,
+      error_message: "Algo de errado aconteceu, tente novamente"
     }
   }
 
@@ -73,23 +76,54 @@ export class RegisterComponent implements OnInit {
   public resetStatus(): void {
     this.status = {
       loading: false,
-      error: false
+      error: false,
+      error_message: "Algo de errado aconteceu, tente novamente"
     };
   }
 
+  public resetCamposVendedor(): void {
+    this.data.cpf = undefined;
+    this.data.telefone = undefined;
+  }
+
+  public resetPassword(): void {
+    this.data.pwd = {
+      password: undefined,
+      confirm_password: undefined
+    }
+  }
+
+  private validaSenha(): boolean {
+    if(this.data.pwd.password != this.data.pwd.confirm_password) {
+      this.resetPassword();
+      return false;
+    }
+
+    return true;
+  }
+  
+
   public submit(): void {
     this.resetStatus();
-    this.status.loading = true;
-    this.userController.create({ ...this.data, password: this.data.pwd.password })
-      .then(res => {
-        this.session.logIn(res.data.auth_token);
-        this.router.navigate(['/profile']);
-      }).catch(err => {
-        this.status.loading = false;
-        this.status.error = true;
-        setTimeout(() => {
-          this.status.error = false;
-        }, 3500);
-      });
+    if(this.validaSenha()) {
+      this.status.loading = true;
+      this.userController.create({ ...this.data, password: this.data.pwd.password })
+        .then(res => {
+          this.session.logIn(res.data.auth_token);
+          this.router.navigate(['/profile']);
+        }).catch(err => {
+          this.status.loading = false;
+          this.status.error = true;
+          setTimeout(() => {
+            this.status.error = false;
+          }, 3500);
+        });
+    } else {
+      this.status.error = true; 
+      this.status.error_message = "As senhas não são iguais. Tente novamente";
+      setTimeout(() => {
+        this.status.error = false;
+      }, 3500);   
+    }
   }
 }
