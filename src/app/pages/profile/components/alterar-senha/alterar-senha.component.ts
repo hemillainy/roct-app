a835cd0d2a310a2219cda2d2322ce6125916e30f
Gabriel <gabriel.almeida.azevedo@ccc.ufcg.edu.br>
2020-10-18 21:08:08 -0300

    alterar-senha component

---
+++ src/app/pages/profile/components/alterar-senha/alterar-senha.component.ts
@@ -0,0 +1,95 @@
+import { animate, style, transition, trigger } from '@angular/animations';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+import { UserService } from 'src/app/controllers/user/user.service';
+
+@Component({
+  selector: 'app-alterar-senha',
+  templateUrl: './alterar-senha.component.html',
+  styleUrls: ['./alterar-senha.component.scss'],
+  animations: [
+    trigger('fadeOut', [
+      transition(':enter', [
+        style({}),
+        animate('250ms', style({})),
+      ]),
+      transition(':leave', [
+        animate('250ms ease-in-out', style({ opacity: 0 }))
+      ])
+    ])
+  ]
+})
+export class AlterarSenhaComponent implements OnInit {
+
+  public data: any;
+
+  public status: any;
+
+  constructor(
+    private userController: UserService,
+    private router: Router
+    ) { 
+    this.data = {
+      pwd: {
+        old_password: undefined,
+        new_password: undefined,
+        confirm_password: undefined
+      },
+      auth_token: undefined,
+    };
+    this.status = {
+      loading: false,
+      error: false,
+      error_message: "Algo de errado aconteceu, tente novamente"
+    }; 
+  }
+
+  public resetStatus(): void {
+    this.status = {
+      loading: false,
+      error: false,
+      error_message: "Algo de errado aconteceu, tente novamente"
+    };
+  }
+
+  public resetPassword(): void {
+    this.data.pwd.new_password = undefined;
+    this.data.pwd.confirm_password = undefined;
+  }
+
+  private validaSenha(): boolean {
+    if (this.data.pwd.new_password != this.data.pwd.confirm_password) {
+      this.resetPassword();
+      return false;
+    }
+
+    return true;
+  }
+
+  public submit(): void {
+    if (this.validaSenha()) {
+      this.status.loading = true;
+      this.userController.create({ ...this.data})
+        .then(res => {
+          this.router.navigate(['/profile']);
+        }).catch(err => {
+          this.status.loading = false;
+          this.status.error = true;
+          setTimeout(() => {
+            this.status.error = false;
+          }, 3500);
+        });
+    } else {
+      this.status.error = true;
+      this.status.error_message = "As senhas não são iguais. Tente novamente";
+      setTimeout(() => {
+        this.status.error = false;
+      }, 3500);
+    }
+  }
+
+  ngOnInit() {
+  }
+
+}
