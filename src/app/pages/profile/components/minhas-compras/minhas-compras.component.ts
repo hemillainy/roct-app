8feda0590960217057b45bf358ea274079a186e0
Vinicius Jorge <vinicius.jorge.silva@ccc.ufcg.edu.br>
2020-10-20 21:38:15 +0000

    atualizar listagem de compras

--- src/app/pages/profile/components/minhas-compras/minhas-compras.component.ts
+++ src/app/pages/profile/components/minhas-compras/minhas-compras.component.ts
@@ -9,57 +9,13 @@ import { Subscription } from 'rxjs';
 })
 export class MinhasComprasComponent implements OnInit {
 
-  constructor(
-    private route: ActivatedRoute,
-    private router: Router
-    ) { }
-
-  ngOnInit() {
-    this.setQueryParams();
-  }
-
-  private setQueryParams(): void {
-    this.subs = this.route.queryParams.subscribe(
-      queryParams => {
-        this.filter.status = queryParams['status'] || '';
-        this.data.page = parseInt(queryParams['page'], 10);
-        if (!this.data.page) {
-          this.data.page = 1;
-        }
-      }
-    );
-
-    //VERIFICAR ESSE ROUTER
-    this.router.navigate([], { queryParams: { page: this.data.page }, queryParamsHandling: 'merge' });
-  }
-
+  // Data
   private subs: Subscription;
   public filter = {
     filtered: false,
-    status: ''
+    status: '',
+    server: ''
   };
-
-  public resetFilter(): void {
-    this.filter = {
-      filtered: false,
-      status: ''
-    };
-    this.search();
-  }
-
-  public clearFilter(): boolean {
-    let result = false;
-    Object.keys(this.filter).slice(1).map(item => {
-      result = result || (this.filter[item] !== '')
-    });
-    return result;
-  }
-
-
-  //AJEITAR ISSO
-  public search(): void {
-    this.filter.filtered = this.clearFilter();
-  }
 
   public data = {
     page: 1,
@@ -165,5 +121,53 @@ export class MinhasComprasComponent implements OnInit {
       }
     ]
   };
+
+  constructor(
+    private route: ActivatedRoute,
+    private router: Router
+  ) { }
+
+  ngOnInit() {
+    this.setQueryParams();
+  }
+
+  private setQueryParams(): void {
+    this.subs = this.route.queryParams.subscribe(
+      queryParams => {
+        this.filter.status = queryParams['status'] || '';
+        this.data.page = parseInt(queryParams['page'], 10);
+        if (!this.data.page) {
+          this.data.page = 1;
+        }
+      }
+    );
+    //VERIFICAR ESSE ROUTER
+    this.router.navigate([], { queryParams: { page: this.data.page }, queryParamsHandling: 'merge' });
+  }
+
+
+
+  public resetFilter(): void {
+    this.filter = {
+      filtered: false,
+      status: '',
+      server: ''
+    };
+    this.search();
+  }
+
+  public clearFilter(): boolean {
+    let result = false;
+    Object.keys(this.filter).slice(1).map(item => {
+      result = result || (this.filter[item] !== '')
+    });
+    return result;
+  }
+
+
+  //AJEITAR ISSO
+  public search(): void {
+    this.filter.filtered = this.clearFilter();
+  }
 
 }
