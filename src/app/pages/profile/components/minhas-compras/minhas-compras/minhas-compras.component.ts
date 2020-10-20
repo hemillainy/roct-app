b381595972bd53699f33306eddd82495687d8df1
Gabriel <gabriel.almeida.azevedo@ccc.ufcg.edu.br>
2020-10-19 21:19:47 -0300

    Componente minhasCompras criado. Rota add

---
+++ src/app/pages/profile/components/minhas-compras/minhas-compras/minhas-compras.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-minhas-compras',
+  templateUrl: './minhas-compras.component.html',
+  styleUrls: ['./minhas-compras.component.scss']
+})
+export class MinhasComprasComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
