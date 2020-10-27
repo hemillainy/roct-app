0cdfa4f93fcc79d2baa4e1890cc0aac692036702
Gabriel <gabriel.almeida.azevedo@ccc.ufcg.edu.br>
2020-10-26 23:46:33 -0300

    Migrando componentes de venda para novo modulo

--- src/app/pages/profile/components/minhas-vendas/minhas-vendas.module.ts
+++ src/app/pages/profile/components/minhas-vendas/minhas-vendas.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MinhasVendasComponent } from './minhas-vendas.component';
 import { VendasComponent } from './vendas/vendas.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -10,6 +11,11 @@ import { VendasComponent } from './vendas/vendas.component';
   ],
   imports: [
     CommonModule,
-  ]
+    FormsModule
+  ],
+  exports: [
+    MinhasVendasComponent,
+    VendasComponent,
+  ],
 })
 export class MinhasVendasModule { }
