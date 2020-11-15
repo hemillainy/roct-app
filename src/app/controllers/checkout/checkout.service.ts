98746364f008b6bdea6a0d90498857cb3f53bcaa
Gabriel <gabriel.almeida.azevedo@ccc.ufcg.edu.br>
2020-11-15 18:04:19 -0300

    Service do checkout

---
+++ src/app/controllers/checkout/checkout.service.ts
@@ -0,0 +1,32 @@
+import { Injectable } from '@angular/core';
+import axios from 'axios';
+import { environment } from 'src/environments/environment';
+import { SessionService } from '../session/session.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CheckoutService {
+
+  private END_POINT: string;
+
+  constructor(
+    private session: SessionService
+  ) {
+    this.END_POINT = environment.END_POINT_API;
+  }
+
+  private getConfig(): any {
+    const token = this.session.getToken();
+    axios.defaults.headers.common.Authorization = token;
+    return { headers: { Authorization: `Bearer ${token}` } };
+  }
+
+  public pay(data: any): Promise<any> {
+    return axios.post(`${this.END_POINT}/purchases/add`, data, this.getConfig());
+  }
+
+  public updateStatusPurchase(data: any): Promise<any> {
+    return axios.post(`${this.END_POINT}/purchases/update_status`, data, this.getConfig());
+  }
+}
