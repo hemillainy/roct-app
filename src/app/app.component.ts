df573fd7de9b6cfc70a61b5f1f8b7c4db305801b
Bruno Rodrigo <bruno@yupistudios.com>
2020-12-07 22:33:36 -0300

    removendo funcao desnecessaria

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -24,10 +24,5 @@ export class AppComponent implements OnInit {
       this.session.logOut();
     }
   }
-
-  detected(isDetected: boolean) {
-    if(isDetected) {
-      alert("Desligue o adblock em nosso site para que ele funcione corretamente.")
-    }
-  }
+  
 }
