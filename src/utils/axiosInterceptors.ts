b1dcc2bdfca5d42f17e4b673248c5dfdee88e7b2
Bruno Meneses <bruno.meneses@ccc.ufcg.edu.br>
2021-03-08 03:07:31 -0300

    Correção no looping de refresh token

--- src/utils/axiosInterceptors.ts
+++ src/utils/axiosInterceptors.ts
@@ -1,36 +1,61 @@
 import axios from "axios";
-import { environment } from 'src/environments/environment';
+import { environment } from "src/environments/environment";
 
-axios.interceptors.response.use((response) => {
-  return response;
+//Rotas que retornam 401 e não precisam disparar o refresh token
+const blacklist = ["changePassword", "login"];
 
-}, (error) => {
-  
-  if (error.response.status !== 401) {
-    return new Promise((resolve, reject) => {
-      reject(error);
-    });
-  }
+axios.interceptors.response.use(
+  (response) => {
+    return response;
+  },
+  (error) => {
+    const config = error.config;
+    const currentPath = new URL(config.url).pathname.split("/");
 
-  return axios.get(environment.END_POINT_API + '/auth/refresh_token', { headers: { Authorization: `Bearer ${localStorage.getItem('refreshToken')}`} })
-    .then((responseToken) => {
-      const token = responseToken.data.token;
+    const notAllowRefrestToken = currentPath.reduce(
+      (accumalator, currentValue) =>
+        accumalator || blacklist.includes(currentValue),
+      false
+    );
 
-      localStorage.setItem('Authorization', token);
-
-      const config = error.config;
-      config.headers['Authorization'] = `Bearer ${token}`;
-
+    if (
+      error.response.status !== 401 ||
+      config.__isRetryRequest ||
+      notAllowRefrestToken
+    ) {
       return new Promise((resolve, reject) => {
-        axios.request(config).then(response => {
-          resolve(response);
-        }).catch((error) => {
-          reject(error);
-        })
+        reject(error);
       });
+    }
 
-    })
-    .catch((error) => {
-      Promise.reject(error);
-    });
-});
+    return axios
+      .get(environment.END_POINT_API + "/auth/refresh_token", {
+        headers: {
+          Authorization: `Bearer ${localStorage.getItem("refreshToken")}`,
+        },
+      })
+      .then((responseToken) => {
+        const token = responseToken.data.token;
+
+        config.__isRetryRequest = true;
+
+        localStorage.setItem("Authorization", token);
+
+        config.headers["Authorization"] = `Bearer ${token}`;
+
+        return new Promise((resolve, reject) => {
+          axios
+            .request(config)
+            .then((response) => {
+              resolve(response);
+            })
+            .catch((error) => {
+              reject(error);
+            });
+        });
+      })
+      .catch((error) => {
+        Promise.reject(error);
+      });
+  }
+);
